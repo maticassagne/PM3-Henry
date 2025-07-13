517442083b8dd9f2593133a384b339bbe0a63f26
MatiCassagne <matiascassagne@gmail.com>
2025-07-13 13:37:38 -0300

    Agregado variables de entorno

--- back/src/config/data-source.ts.ts
+++ back/src/config/data-source.ts.ts
@@ -1,13 +1,14 @@
 import { DataSource } from "typeorm";
 import { User } from "../entities/User";
+import { DBNAME, DBPORT, DBPASS, DBUSER } from "./envs";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "4571679",
-  database: "demo_typeorm",
+  port: DBPORT,
+  username: DBUSER,
+  password: DBPASS,
+  database: DBNAME,
   // dropSchema: true,
   synchronize: true,
   logging: ["error"],
--- back/src/config/envs.ts
+++ back/src/config/envs.ts
@@ -1,3 +1,7 @@
 import "dotenv/config";
 
 export const PORT = process.env.PORT;
+export const DBUSER = process.env.DBUSER;
+export const DBPASS = process.env.DBPASS;
+export const DBPORT = Number(process.env.DBPORT);
+export const DBNAME = process.env.DBNAME;
